a3984339eedc8dfc0ada3d096ac31f070fc9b231
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-10 22:02:55 +0600

    added Statistics component and set route for this

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../components/Home/Home";
+import Statistics from "../components/Statistics/Statistics";
 import Main from "../layouts/Main";
 
 const router = createBrowserRouter([
@@ -9,6 +10,7 @@ const router = createBrowserRouter([
         children: [
             { path: '/', element: <Home></Home> },
             { path: '/home', element: <Home></Home> },
+            { path: '/statistics', element: <Statistics></Statistics> },
         ]
     }
 ]);
