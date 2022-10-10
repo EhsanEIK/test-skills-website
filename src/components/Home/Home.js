3a322f505b072fd38350274d9c7e7f79a7104ec9
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-10 22:44:24 +0600

    added header in Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-
-        </div>
+        <header className='header flex'>
+            <div className='text-white md:text-start text-center md:w-2/4 mt-14 md:ml-10 mx-3'>
+                <h1 className='md:text-5xl text-3xl mb-5'>Welcome To
+                    <span className='text-amber-600 font-bold mx-3'>TEST SKILLS</span>
+                    <br />Web App!!!
+                </h1>
+                <p>This is an Quiz Test website. Here you can test your skills on your technology knowledge. This is an very popular website in the world now! So, why are you waiting?!! Enjoy the quiz and check you ability.</p>
+            </div>
+        </header>
     );
 };
 
