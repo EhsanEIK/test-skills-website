833abb896994e501a10194030a28f03a01757ccd
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-11 13:41:30 +0600

    added blog-3 in Blogs

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -21,7 +21,13 @@ const Blogs = () => {
                     Context API is a new feature added in version 16.3 of React that allows one to share state across the entire app lightly and with ease. The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. The useContext hook allows us to connect and consume a context. The useContext hook receives a single argument, which is the context that you want to have access to.
                 </p>
             </div>
-
+            {/* blog-03 */}
+            <div className='border-2 rounded-lg my-10 px-10 py-6'>
+                <h1 className='text-2xl font-bold mb-3'>Explain about useRef().</h1>
+                <p>
+                    The useRef hook is the new addition in React 16.8. The useRef Hook allows us to persist values between renders. The hook useRef() accepts one argument, which is the value to initialize the property current in the returned object. It can be used to access a DOM element directly.
+                </p>
+            </div>
         </div>
     );
 };
