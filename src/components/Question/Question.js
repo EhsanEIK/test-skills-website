5e16bc8d9dec349d06c8a719f7076e2941f9e942
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-11 10:10:44 +0600

    added toastify for giving corrent ans/wrong ans alert

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Question = ({ questionDetails }) => {
-    const { question, options } = questionDetails;
-    // console.log(options)
+    const { id, question, options, correctAnswer } = questionDetails;
+
+    const checkAnswer = (optionsId) => {
+        console.log(optionsId)
+        document.getElementById(optionsId).value === correctAnswer ? toast.success("Corrent Answer!") : toast.error("Wrong Answer!");
+    }
 
     return (
         <div className='border-2 p-5 mb-10'>
@@ -11,17 +17,18 @@ const Question = ({ questionDetails }) => {
             {/* showed all the options of each questions */}
             <div className='grid md:grid-cols-2 grid-cols-1 gap-5'>
                 {
-                    options.map((option, id) => {
-                        const keyId = id + option.slice(0, 10);
+                    options.map((option, idx) => {
+                        const optionId = idx + option.slice(0, 10);
                         return (
-                            <div key={keyId} className="flex items-center mb-4" >
-                                <input id={`${keyId}`} type="radio" name={question} value={option} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300" />
-                                <label htmlFor={`${keyId}`} className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300" > {option}</label>
+                            <div onClick={() => checkAnswer(optionId)} key={optionId} className="flex items-center mb-4" >
+                                <input id={optionId} type="radio" name={id} value={option} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300" />
+                                <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300" >{option}</label>
                             </div>
                         )
                     })
                 }
-            </div >
+            </div>
+            <ToastContainer></ToastContainer>
         </div >
     );
 };
