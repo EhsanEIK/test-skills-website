ef127ccfba4972c410cd11105d5749bf8805bbf8
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-11 18:58:04 +0600

    updated footer content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className='bg-red-500 w-full fixed bottom-0 px-5 py-3 mt-10'>
-            <p className='text-md text-white text-center'>&copy; Developed by <a href="https://www.linkedin.com/in/ehsanulislamkhan/" target="_blank" rel="noopener noreferrer" className='text-amber-400 font-semibold'>Ehsan</a></p>
+            <p className='text-md text-white text-center'>&copy; 2022 - Developed by <a href="https://www.linkedin.com/in/ehsanulislamkhan/" target="_blank" rel="noopener noreferrer" className='text-amber-400 font-semibold'>Ehsan</a></p>
         </footer>
     );
 };
