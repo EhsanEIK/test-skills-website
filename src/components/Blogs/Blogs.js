555726d2e236f58ba3fce810a56b98f6b540276b
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-11 13:40:34 +0600

    added blog-2 in Blogs

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -14,6 +14,14 @@ const Blogs = () => {
                     React Router is a standard library for routing in React. It is used to create routing in the React application using React Router Package.
                 </p>
             </div>
+            {/* blog-02 */}
+            <div className='border-2 rounded-lg my-10 px-10 py-6'>
+                <h1 className='text-2xl font-bold mb-3'>How does Context API work?</h1>
+                <p>
+                    Context API is a new feature added in version 16.3 of React that allows one to share state across the entire app lightly and with ease. The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. The useContext hook allows us to connect and consume a context. The useContext hook receives a single argument, which is the context that you want to have access to.
+                </p>
+            </div>
+
         </div>
     );
 };
